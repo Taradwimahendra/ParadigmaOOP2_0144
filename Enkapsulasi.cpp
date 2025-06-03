6d9937790b97944954e17ddb043587dfd7b425f3
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-06-03 18:37:04 +0700

    definisi setSaklarNo pada akses modifier public

--- Enkapsulasi.cpp
+++ Enkapsulasi.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 class remoteLampu {
     private:
+        string SaklarNo[10];
+      public: 
+        void setSaklarNo(int i, string value) {
+            SaklarNo[i] = value;
+        }
+    string getSaklarNo(int i) {
+        return SaklarNo[i];
+    }
 };
