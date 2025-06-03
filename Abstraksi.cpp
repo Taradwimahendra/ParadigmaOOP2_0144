448c7801005df61c00e51eb9b430c3e473a2bfd6
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-06-03 18:07:22 +0700

    Memnampilkan nilai x dan y

--- Abstraksi.cpp
+++ Abstraksi.cpp
@@ -9,5 +9,10 @@ class AbstraksiKlas {
                 x = a;
                 y = b;
             }
+            
+    void display(){
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+        }
 
 };
